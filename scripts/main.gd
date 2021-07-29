30a394bcd1f12c1e3eea76df4806e8e2a4336b60
Julian Holfeld <julianholfeld@student.uni-kassel.de>
2021-07-29 10:26:52 +0200

    feat: added sounds and simple beat-pattern

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,24 +4,48 @@ onready var up_label = $CenterContainer/VBoxContainer/Up
 onready var left_label = $CenterContainer/VBoxContainer/HBoxContainer/Left
 onready var right_label = $CenterContainer/VBoxContainer/HBoxContainer/Right
 onready var down_label = $CenterContainer/VBoxContainer/Down
+onready var input_player = $InputSound
+onready var beat_player = $Beat
+onready var beat_timer = $BeatTimer
+
+var up_bell = preload("res://sounds/c-bell.wav")
+var left_bell = preload("res://sounds/d-bell.wav")
+var right_bell = preload("res://sounds/e-bell.wav")
+var down_bell = preload("res://sounds/f-sharp-bell.wav")
+
+var bpm = 120
+var section_time = 120 / 60
+var input_allowed = false
 
 
 func _ready():
+	beat_timer.wait_time = section_time
 	_reset_labels()
 
 
-func _process(delta):
-	_check_input(up_label, "ui_up")
-	_check_input(left_label, "ui_left")
-	_check_input(right_label, "ui_right")
-	_check_input(down_label, "ui_down")
+func _process(_delta):
+	if input_allowed:
+		_check_input()
 
 
-func _check_input(label, key):
-	if Input.is_action_just_pressed(key):
-		_reset_labels()
-		key.erase(0, 3)
-		label.text = key
+func _check_input():
+	_reset_labels()
+	if Input.is_action_just_pressed("ui_up"):
+		up_label.text = "up"
+		input_player.stream = up_bell
+		input_player.play()
+	elif Input.is_action_just_pressed("ui_left"):
+		left_label.text = "left"
+		input_player.stream = left_bell
+		input_player.play()
+	elif Input.is_action_just_pressed("ui_right"):
+		right_label.text = "right"
+		input_player.stream = right_bell
+		input_player.play()
+	elif Input.is_action_just_pressed("ui_down"):
+		down_label.text = "down"
+		input_player.stream = down_bell
+		input_player.play()
 
 
 func _reset_labels():
@@ -29,3 +53,10 @@ func _reset_labels():
 	left_label.text = ""
 	right_label.text = ""
 	down_label.text = ""
+
+
+func _on_BeatTimer_timeout():
+	input_allowed = !input_allowed
+	if !input_allowed:
+		_reset_labels()
+		beat_player.play()
